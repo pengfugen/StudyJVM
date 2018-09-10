1a88b583b294324238bf563c1e9ca2a349e3312b
pengfugen <pfg0717@163.com>
2018-09-10 14:48:14 +0800

    update

--- src/com/pfg/GCRoot.java
+++ src/com/pfg/GCRoot.java
@@ -10,17 +10,46 @@ package com.pfg;
 public class GCRoot {
 	
 	private static final int _1MB = 1024*1024;
-	private byte[] bigSize = new byte[80 * _1MB];
+	private byte[] memory;
 	
-	// 可作为GC Root的第一种情况
+	GCRoot(int size) {
+		memory = new byte[size * _1MB];
+	}
+	
+	// GC Root的第一种情况
 	private static void method01() {
-		GCRoot root = new GCRoot();
+		GCRoot root1 = new GCRoot(80);
 		System.gc();
 	}
 	
+	// GC Root的第二种情况
+	static GCRoot root2;
+	
+	// GC Root的第三种情况
+	private final static GCRoot root3 = new GCRoot(80);
+	
 	public static void main(String[] args) {
-		method01();
+		
+		/* 
+		 // GC Root的第一种情况
+		 method01();
+		 System.gc();
+		 */
+		
+		/*
+	     // GC Root的第二种情况
+		 GCRoot root = new GCRoot(40);
+		 root = null;
+		 System.gc();
+		
+		 root.root2 = new GCRoot(80);
+		*/
+		
+		// GC Root的第三种情况
+		GCRoot root = new GCRoot(40);
+		root= null;
 		System.gc();
+		
 	}
 
 }
