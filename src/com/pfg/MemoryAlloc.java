bfed72310c7536100920e938e57e867c49b5f0e5
pengfugen <pfg0717@163.com>
2018-09-13 17:38:16 +0800

    update

--- src/com/pfg/MemoryAlloc.java
+++ src/com/pfg/MemoryAlloc.java
@@ -3,17 +3,46 @@ package com.pfg;
 public class MemoryAlloc {
 	private static final int _1MB = 1024*1024;
 	
+	byte [] mallocation1 = new byte[_1MB / 4];
+	byte [] mallocation2 = new byte[_1MB / 4];
+	byte [] mallocation3 = new byte[4* _1MB];
+	byte [] mallocation4 = new byte[3* _1MB];
+	
+	
 	public static void testAllocation() {
 		byte [] allocation1, allocation2, allocation3, allocation4;
-		allocation1 = new byte[2 * _1MB];
-		allocation2 = new byte[2 * _1MB];
-		allocation3 = new byte[2 * _1MB];
-		allocation4 = new byte[4 * _1MB]; // 这里并没有发生Minor GC，而是直接分配到了老年代了
+		allocation1 = new byte[_1MB / 4];
+		allocation2 = new byte[_1MB / 4];
+		allocation3 = new byte[3* _1MB];
+		allocation3 = null;
+		allocation3 = new byte[4* _1MB]; 
+	}
+	
+	public static void testTenuringThreshold() {
+		byte [] allocation1, allocation2, allocation3;
+		allocation1 = new byte[_1MB / 4];
+		allocation2 = new byte[3 * _1MB];
+		allocation3 = new byte[3 * _1MB];
+		allocation3 = null;
+		allocation3 = new byte[3 * _1MB]; 
+	}
+	
+	public static void testTenuringThreshold2() {
+		byte [] allocation1, allocation2, allocation3, allocation4;
+		allocation1 = new byte[_1MB / 4];
+		allocation2 = new byte[_1MB / 4];
+		allocation3 = new byte[3 * _1MB];
+		allocation4 = new byte[3 * _1MB]; 
+		allocation4 = null;
+		allocation4 = new byte[3* _1MB];
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		testAllocation();
+		//testAllocation();
+		testTenuringThreshold2();
+		//MemoryAlloc alloc = new MemoryAlloc();
+		//testAllocation();
 	}
 
 }
